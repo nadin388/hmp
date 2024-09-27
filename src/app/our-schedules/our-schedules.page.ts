6a0a813d0b5e504e21ce07e9d6c65fd2690424bd
cennialieta <s160422037@student.ubaya.ac.id>
2024-09-27 11:12:17 +0700

    hmp kacau

--- src/app/our-schedules/our-schedules.page.ts
+++ src/app/our-schedules/our-schedules.page.ts
@@ -9,35 +9,35 @@ export class OurSchedulesPage implements OnInit {
 
   ourSchedules = [
     {
-      date : '05 - 09 - 2024',
+      date : '05-09-2024',
       nameEvent : 'Regional Qualifer - Valorant',
       nameGame : "Valorant",
       nameTeam : 'Team A',
     },
 
     {
-      date : '10 - 09 - 2024',
+      date : '10-09-2024',
       nameEvent : 'League of Legends Workshop',
       nameGame : "LOL",
-      nameTeam : 'Team C',
+      nameTeam : 'Team Cs',
     },
 
     {
-      date : '07 - 10 - 2024',
+      date : '07-10-2024',
       nameEvent : 'Call of Duty Championship',
       nameGame : "COD",
       nameTeam : 'Team A',
     },
 
     {
-      date : '11 - 11 - 2024',
+      date : '11-11-2024',
       nameEvent : 'Dota 2 Livestream',
       nameGame : "Dota 2",
       nameTeam : 'Team B',
     },
 
     {
-      date : '04 - 12 - 2024',
+      date : '04-12-2024',
       nameEvent : 'Fortnite Invitational',
       nameGame : "Fortnite",
       nameTeam : 'Team A',
