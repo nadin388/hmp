70ec59072ac565dae1d1571c6621d07aa636f89c
nadin388 <s160422027@student.ubaya.ac.id>
2024-10-12 12:40:32 +0700

    lalala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  dropdownOpen:boolean = false;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
   constructor() {}
 }
