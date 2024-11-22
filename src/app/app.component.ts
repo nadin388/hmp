f7fcd9df4ca6c88e101d37b456276e8214dbdd03
sarahlauras <s160422015@student.ubaya.ac.id>
2024-11-22 07:48:44 +0700

    don

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-//import { defineCustomElements } from '@ionic/pwa-elements/loader'
+import { defineCustomElements } from '@ionic/pwa-elements/loader'
 import { UserserviceService } from './userservice.service';
 
 @Component({
@@ -8,39 +8,36 @@ import { UserserviceService } from './userservice.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  fname=""
-  username=""
-  password=""
+  fname = ""
+  username = ""
+  password = ""
 
   constructor(private userservice: UserserviceService) {
-    this.fname=localStorage.getItem("app_fname") || ""
-    this.username=localStorage.getItem("app_username") || ""
+    this.fname = localStorage.getItem("app_fname") || ""
+    this.username = localStorage.getItem("app_username") || ""
 
-    //defineCustomElements(window)
-  
+    defineCustomElements(window)
   }
 
-  dropdownOpen:boolean = false;
+  dropdownOpen: boolean = false;
 
   toggleDropdown() {
     this.dropdownOpen = !this.dropdownOpen;
   }
 
-  login()
-{
-   this.userservice.login(this.username,this.password).subscribe(
-     (response: any) => {
-        if(response.result==='success'){
+  login() {
+    this.userservice.login(this.username, this.password).subscribe(
+      (response: any) => {
+        if (response.result === 'success') {
           alert("success")
-          this.fname=response.fname;
-          localStorage.setItem("app_fname",this.fname)
-          localStorage.setItem("app_username",this.username)
-         }
-         else
-         {
-            alert(response.message)
-         }
-  });
-}
+          this.fname = response.fname;
+          localStorage.setItem("app_fname", this.fname)
+          localStorage.setItem("app_username", this.username)
+        }
+        else {
+          alert(response.message)
+        }
+      });
+  }
 
 }
